5fcde76657290b904debfbb56b2381098419a6ed
RyanP <tupn1702@gmail.com>
2022-11-27 20:16:27 +1100

    Adding validation error

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -8,14 +8,28 @@ namespace MvcMovie.Models
 	public class Movie
 	{
 		public int Id { get; set; }
+		[StringLength(50, MinimumLength =3)]
+		[Required]
 		public string? Title { get; set; }
 
 		[Display(Name = "Release Date")]
 		[DataType(DataType.Date)]
-		public DateTime ReleaseDate { get; set; }
+        //[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+
+		[RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+		[Required]
+		[StringLength(30)]
 		public string? Genre { get; set; }
 
-		[Column(TypeName = "decimal(18, 2)")]
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
 		public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
+        public string? Rating { get; set; }
 	}
 }
--- MvcMovie/Models/SeedData.cs
+++ MvcMovie/Models/SeedData.cs
@@ -22,28 +22,32 @@ namespace MvcMovie.Models
 						Title = "When Harry Met Sally",
 						ReleaseDate = DateTime.Parse("1989-2-12"),
 						Genre = "Romantic Comedy",
-						Price = 7.99M
+                        Rating = "R",
+                        Price = 7.99M
 					},
 					new Movie
 					{
 						Title = "Ghostbusters ",
 						ReleaseDate = DateTime.Parse("1984-3-13"),
 						Genre = "Comedy",
-						Price = 8.99M
+                        Rating = "M",
+                        Price = 8.99M
 					},
 					new Movie
 					{
 						Title = "Ghostbusters 2",
 						ReleaseDate = DateTime.Parse("1986-2-23"),
 						Genre = "Comedy",
-						Price = 9.99M
+                        Rating = "O",
+                        Price = 9.99M
 					},
 					new Movie
 					{
 						Title = "Rio Bravo",
 						ReleaseDate = DateTime.Parse("1959-4-15"),
 						Genre = "Western",
-						Price = 3.99M
+                        Rating = "E",
+                        Price = 3.99M
 					}
 				);
 				context.SaveChanges();
